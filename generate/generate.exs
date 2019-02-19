77fab22241f504eb1ed289be747c62a38ff2c61b
Boshen <boshenc@gmail.com>
2019-02-19 10:15:12 +0800

    chore(generate): replace loop insert with batch insert [ci skip]

--- generate/generate.exs
+++ generate/generate.exs
@@ -1,34 +1,55 @@
-alias Twitter.Accounts
 alias Twitter.Accounts.User
+alias Twitter.Accounts.Follower
 alias Twitter.Repo
-alias Twitter.Resources
+alias Twitter.Resources.Tweet
 
 Faker.start()
 
 me = Repo.get_by(User, name: "Superuser")
 
-followers_count = 10
-tweets_count = 1000
+followers_count = 100
+tweets_count = 10000
+
+utc_datetime_usec = fn ->
+  DateTime.from_unix!(System.system_time(:microsecond), :microsecond)
+end
 
 # create users
 followers =
-  Enum.map(0..10, fn _i ->
-    Accounts.create_user(%{name: Faker.Name.name()})
-    |> elem(1)
+  Enum.map(1..followers_count, fn _i ->
+    %{
+      name: Faker.Name.name(),
+      inserted_at: utc_datetime_usec.(),
+      updated_at: utc_datetime_usec.()
+    }
   end)
+
+{_count, followers} = Repo.insert_all(User, followers, returning: [:id])
 
 # follow users
-Enum.each(followers, fn follower ->
-  Accounts.follow_user(me, follower.id)
-end)
+followings =
+  Enum.map(followers, fn follower ->
+    %{
+      user_id: me.id,
+      follower_id: follower.id,
+      inserted_at: utc_datetime_usec.(),
+      updated_at: utc_datetime_usec.()
+    }
+  end)
+
+Repo.insert_all(Follower, followings, returning: [:id])
 
 # each follower creates tweets
-
 Enum.each(followers, fn follower ->
-  Enum.each(0..tweets_count, fn _i ->
-    Resources.create_tweet(%{
-      message: Faker.Lorem.sentence(),
-      creator_id: follower.id
-    })
-  end)
+  tweets =
+    Enum.map(1..tweets_count, fn _i ->
+      %{
+        message: Faker.Lorem.sentence(),
+        creator_id: follower.id,
+        inserted_at: utc_datetime_usec.(),
+        updated_at: utc_datetime_usec.()
+      }
+    end)
+
+  Repo.insert_all(Tweet, tweets)
 end)
